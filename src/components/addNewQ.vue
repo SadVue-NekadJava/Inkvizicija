d6d409f520704856a68b750b0565eb7ef3431d01
lazar <lazarbajic@hotmail.com>
2018-09-07 16:06:38 +0200

    nije responsive,dodaj pitanja

--- src/components/addNewQ.vue
+++ src/components/addNewQ.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-
+<label @click="nazad" class="nazad">Nazad</label>
+<label class="sacuvaj" @click="sacuvajQ">Sacuvaj pitanje</label>
 
   <form class="">
     <div class="pitanje">
@@ -48,6 +49,9 @@ export default {
   },
   methods:{
 
+      nazad(){
+        location.reload();
+      }
   },
   computed:{
 
@@ -57,6 +61,55 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
+.nazad{
+  text-decoration: none;
+  font: 400 1em/1.5 "Neuton";
+  letter-spacing: 0;
+  // padding: .25em 0 .325em;
+  display: block;
+  margin: 0 auto;
+  text-shadow: 0 0 80px rgba(255,255,255,.5);
+  text-transform: uppercase;
+  letter-spacing: .5em;
+  display: inline-block;
+  border: 4px double rgba(255,255,255,.5);
+  border-width: 4px 0;
+  padding: 1.5em 0em;
+  width: 350px;
+  margin: 0 auto;
+  color: rgba(255,255,255,.5);
+  position: absolute;
+     top: 200px;
+     left: 200px;
+}
+
+.sacuvaj{
+  font: 400 1em/1.5 "Neuton";
+  letter-spacing: 0;
+  // padding: .25em 0 .325em;
+  display: block;
+  margin: 0 auto;
+  text-shadow: 0 0 80px rgba(255,255,255,.5);
+  text-transform: uppercase;
+  letter-spacing: .5em;
+  display: inline-block;
+  border: 4px double rgba(255,255,255,.5);
+  border-width: 4px 0;
+  padding: 1.5em 0em;
+  width: 350px;
+  margin: 0 auto;
+  color: rgba(255,255,255,.5);
+  position: absolute;
+     top: 200px;
+     right: 200px;
+}
+
+
+
+.nazad:hover { transform: scale(1.3); }
+.sacuvaj:hover { transform: scale(1.3); }
+
 .radioDiv{
   margin: 10px;
 }
@@ -122,98 +175,10 @@ color: white;
   }
 }
 
-.clicker{
-   font: 400 1em/1.5 "Neuton";
-  color: rgba(255,255,255,.25);
-  display: block;
-  margin: 0 auto;
-  text-shadow: 0 0 80px rgba(255,255,255,.5);
-  text-transform: uppercase;
-  letter-spacing: .5em;
-  display: inline-block;
 
 
 
-  letter-spacing: 0;
-  padding: .25em 0 .325em;
-  background-color:  #090d00;
-  outline: none;
-  font-size: 16px;
 
-	  position:absolute;
-  cursor: pointer;
-  padding: 0;
-	  border: none;
-	  height: 103px;
-	  width: 103px;
-  left: 8px;
-  top: 8px;
-	  border-radius: 100px;
-	  z-index: 2;
-}
-
-.clicker:active{
-	  transform: translate(0, 1px);
-  height: 100px;
-	  box-shadow: 0px 1px 0 0 rgb(190,190,190) inset;
-}
-
-.circle{
-	  position: relative;
-  border-radius:65px;
-	  width: 120px;
-	  height: 120px;
-	  z-index: 1;
-}
-
-.circle.third{
-  border-radius: 0;
-}
-
-.clicker:hover{
-  color: white;
-}
-
-.clicker:hover + .circle, .clicker:active + .circle {
-	  animation: rotator linear 1.5s infinite;
-
-}
-
-@keyframes rotator{
-	  from{ transform: rotate(0deg); }
-	  to{ transform: rotate(360deg); }
-}
-
-
-.angled {
-			background-image: linear-gradient(45deg,
-                white 0%,
-                white 30%,
-                #ff7100  30%,
-								#ff5200   70%,
-                white 70%,
-                white 100%);
-		}
-
-.angled.second{
-  			background-image: linear-gradient(
-                white 0%,
-                white 30%,
-                rgb(250,160,120) 30%,
-								rgb(250,160,120) 70%,
-                white 70%,
-                white 100%);
-}
-
-.angled.third{
-			background-image: linear-gradient(45deg,
-                white 0%,
-                white 30%,
-                rgb(130,230,135) 30%,
-								rgb(130,230,135) 70%,
-                white 70%,
-                white 100%);
-}
 
 .zadnji{
 margin-top: 0;
