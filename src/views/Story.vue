4b6f0bbf36fb270664cf47dcc559213131ac373d
markocopic <markocopicpp@gmail.com>
2018-09-13 13:22:11 +0200

    pokusaj

--- src/views/Story.vue
+++ src/views/Story.vue
@@ -365,7 +365,7 @@ export default {
     var strana =  Number(window.localStorage.getItem("story"));
     window.localStorage.setItem("story", (strana+1));
     console.log(window.localStorage.getItem("story"));
-    if(window.localStorage.getItem("story")>2){
+    if(window.localStorage.getItem("story")>1){
       this.$router.push('/mainPage');
     }
     //upisivanje poena i zlata u bazu
